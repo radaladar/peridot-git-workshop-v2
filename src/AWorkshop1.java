9a5717542c792cb51f4b4036c6a9b4a3f5ad669d
Bedi Szilvia <bedi.szilvia@gmail.com>
2017-01-12 16:12:17 +0100

    create conflict

--- src/AWorkshop1.java
+++ src/AWorkshop1.java
@@ -4,7 +4,7 @@
 public class AWorkshop1 {
 
     public void greet(String name){
-        System.out.println("Greetings!");
+        System.out.println("Greetings Everyone! Great to work together!");
     }
 
 }
