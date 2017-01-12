80d77e806448ff32bdf5c577e9bb4d0eadf8f8b4
PappBeata <pappbeata@gmail.com>
2017-01-12 16:19:57 +0100

    d5

--- src/BWorkshop1.java
+++ src/BWorkshop1.java
@@ -1,7 +1,7 @@
 /// Created by BB on 2017-01-12.
 
 public class BWorkshop1 {
-    
+
     public void count(String month, long day, int xyyxyxyxyxxxx) {
         month = "december";
         System.out.println("I was born in " + month + day + xyyxyxyxyxxxx);
